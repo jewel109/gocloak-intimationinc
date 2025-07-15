11ab5ef5817483b09a1bbb8db78621cb9ad33e08
jewel109 <raihanurrahmanjewel@gmail.com>
2025-07-15 23:14:20 +0600

    all working now

--- main.go
+++ main.go
@@ -1,23 +1,57 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"net"
 	"net/http"
+	"os"
 
-	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"go.uber.org/fx"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-func main() {
-	r := gin.Default()
+func NewHTTPServer(lc fx.Lifecycle) *http.Server {
+	srv := &http.Server{Addr: ":8080"}
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			ln, err := net.Listen("tcp", srv.Addr)
+			if err != nil {
+				return err
+			}
+			fmt.Println("Starting HTTP server at the port  ", srv.Addr)
+			go srv.Serve(ln)
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			return srv.Shutdown(ctx)
+		},
+	})
+	return srv
+}
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong hamid jewel uday sagor",
-		})
-	})
-	r.GET("/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "world",
-		})
-	})
-	r.Run(":8080")
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
+	dbport := os.Getenv("DB_PORT")
+	mysqldb := os.Getenv("MYSQL_DATABASE")
+	dbuser := os.Getenv("MYSQL_USER")
+	dbpass := os.Getenv("MYSQL_PASSWORD")
+	dbhost := "127.0.0.1"
+	fmt.Println(port, dbport, dbpass, dbuser, mysqldb)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+		dbuser, dbpass, dbhost, dbport, mysqldb)
+
+	fmt.Println(dsn)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	fmt.Println("db connected,%s", db)
 }
